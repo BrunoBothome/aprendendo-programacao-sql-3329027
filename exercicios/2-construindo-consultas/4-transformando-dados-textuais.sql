22cf33e29a3cea35d5d10d183e1b2c82c27e748b
BrunoBothome <105000688+BrunoBothome@users.noreply.github.com>
2024-01-25 16:58:34 +0000

    Manipulação d estring nas consultas de dados

--- exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
+++ exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
@@ -1,16 +1,55 @@
--- Crie uma consulta que retorne as colunas CustomerId, FirstName, LastName e Address das pessoas consumidoras brasileiras
+-- Crie uma consulta que retorne as colunas CustomerId, 
+-- FirstName, LastName e Address das pessoas consumidoras 
+-- brasileiras
 
+SELECT DISTINCT CustomerId as id, FirstName as Nome,
+LastName as SobreNome, Address as Endereco
+FROM customers WHERE Country LIKE 'Brazil';
 
--- Na consulta anterior, converta o sobrenome para letras maiúsculas
+-- Na consulta anterior, converta o sobrenome para 
+-- letras maiúsculas
 
+SELECT DISTINCT CustomerId as id, FirstName as Nome,
+UPPER (LastName) as SobreNome, Address as Endereco
+FROM customers WHERE Country LIKE 'Brazil';
 
--- Na consulta anterior, converta o nome para letras minúsculas
+-- Na consulta anterior, converta o nome para letras
+-- minúsculas
 
+SELECT DISTINCT CustomerId as id, LOWER (FirstName) as Nome,
+UPPER (LastName) as SobreNome, Address as Endereco
+FROM customers WHERE Country LIKE 'Brazil';
 
--- Na consulta anterior, adicione uma coluna cujos os dados seja o resultado da concatenação das colunas de nome e sobrenome
+-- Na consulta anterior, adicione uma coluna cujos os 
+-- dados seja o resultado da concatenação das colunas
+-- de nome e sobrenome
 
+SELECT DISTINCT 
+CustomerId as id, 
+LOWER (FirstName) as Nome,
+UPPER (LastName) as SobreNome,
+FirstName || ' ' || LastName as NomeCompleto, 
+Address as Endereco
+FROM customers WHERE Country LIKE 'Brazil';
 
--- Na consulta anterior, substitua "Av." por "Avenida" na coluna endereço
+-- Na consulta anterior, substitua "Av." por "Avenida"
+-- na coluna endereço
 
+SELECT DISTINCT 
+CustomerId as id, 
+LOWER (FirstName) as Nome,
+UPPER (LastName) as SobreNome,
+FirstName || ' ' || LastName as NomeCompleto, 
+REPLACE(Address, 'Av.', 'Avenida') as Endereco
+FROM customers WHERE Country LIKE 'Brazil';
 
--- Na consulta anterior, utilize o símbolo de coringa para construir padrões textuais
+-- Na consulta anterior, utilize o símbolo de coringa
+-- para construir padrões textuais
+
+SELECT DISTINCT 
+CustomerId as id, 
+LOWER (FirstName) as Nome,
+UPPER (LastName) as SobreNome,
+FirstName || ' ' || LastName as NomeCompleto, 
+REPLACE(Address, 'Av.', 'Avenida') as Endereco
+FROM customers WHERE Country LIKE 'Bra%';
