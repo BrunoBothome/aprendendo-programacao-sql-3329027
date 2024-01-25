e47766226bdf3271eb2d74db501d356a21ec571e
BrunoBothome <105000688+BrunoBothome@users.noreply.github.com>
2024-01-25 16:41:26 +0000

    Consulta alterando informações dos dados

--- exercicios/2-construindo-consultas/3-condicional.sql
+++ exercicios/2-construindo-consultas/3-condicional.sql
@@ -1,2 +1,12 @@
--- Escreva uma consulta que retorne o nome por extenso dos estados brasileiros
+-- Escreva uma consulta que retorne o nome por extenso 
+-- dos estados brasileiros
+
+SELECT DISTINCT Country as Pais, State as Estado_Sigla,
+CASE
+  WHEN state = 'SP' THEN 'São Paulo'
+  WHEN State = 'RJ' THEN 'Rio de Janeiro'
+  ELSE 'Estado Desconhecido'
+END AS Estado
+FROM Customers
+WHERE Country LIKE 'Brazil';
 
